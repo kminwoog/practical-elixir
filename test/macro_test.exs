6c9a417b6feee125ffb653b683987d2ef2f78456
kminwoog <kminwoog@gmail.com>
2018-08-23 23:15:08 +0900

    add minor macro test

--- test/macro_test.exs
+++ test/macro_test.exs
@@ -7,9 +7,22 @@ defmodule MacroTest do
     end
   end
 
+  defmacro cond_msg(cond?, msg) do
+    m = Macro.to_string(cond?)
+
+    quote do
+      result = unquote(cond?)
+      "#{unquote(m)} is #{inspect(result)}, #{unquote(msg)}"
+    end
+  end
+
   test "macro expand" do
     value = 2
     ret = get_msg("unquote value = #{value}")
     assert ret == "unquote value = 2"
+
+    a = 2
+    ret = cond_msg(a == 2, "a's value = #{a}")
+    assert ret == "a == 2 is true, a's value = 2"
   end
 end
